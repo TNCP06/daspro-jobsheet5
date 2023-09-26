be677f140bf3da61b67e76821ac3c9479cabcd5a
TNCP06 <ctionusa6@gmail.com>
2023-09-26 10:10:47 +0700

    Percobaan 1

---
+++ New folder/PemilihanPercobaan127.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PemilihanPercobaan127 {
+    public static void main(String[] args) {
+        Scanner input27 = new Scanner(System.in);
+        int angka;
+
+        System.out.print("Masukkan angka: ");
+        angka = input27.nextInt();
+
+        String output = (angka%2==0) ? "Angka "+angka+" bilangan genap":"Angka "+angka+" bilangan ganjil";
+        System.out.println(output);
+
+        input27.close();
+    }
+}
